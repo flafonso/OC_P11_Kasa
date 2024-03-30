855f76c7acf40fd1cd37a2dff34e7270f1ac3836
Flavien Afonso <af.flavien@gmail.com>
2024-03-30 01:10:01 +0100

    I'm starting to display more data on the housing page

--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import Collapse from "../../components/Collapse";
 import Slideshow from "../../components/Slideshow";
 
 function Housing() {
@@ -7,8 +8,32 @@ function Housing() {
   console.log(ad);
 
   return (
-    <div>
+    <div className="housing">
       <Slideshow pictures={ad.pictures} />
+      <div className="housing__informations">
+        <h1 className="housing__title">{ad.title}</h1>
+        <p className="housing__location">{ad.location}</p>
+        <div className="housing__tags">
+          {ad.tags.map((tag, i) => (
+            <div key={`${tag}-${i}`} className="tag">
+              {tag}
+            </div>
+          ))}
+        </div>
+        <Collapse title="Descrition" content={ad.description} />
+        <Collapse title="Équipement" content="" />
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
